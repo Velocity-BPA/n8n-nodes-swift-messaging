4d601da513c033091a94849c33d970a88d8e14ea
John Drefahl <john@velobpa.com>
2026-04-03 02:15:16 -0500

    chore: add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
+ * Licensed under the Business Source License 1.1
+ */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  testTimeout: 30000,
+};
